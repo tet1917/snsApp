d6e4d8d52bc7bcbff0b84e7112d1ae0430863e43
tet1917 <tetsu0224.soccer@icloud.com>
2023-09-24 23:45:37 +0900

    routes

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 const express = require("express");
 const app = express();
+const userRoute = require("./routes/users");
+const authRoute = require("./routes/auth");
+const postRoute = require("./routes/post");
 const PORT = 3000;
 
-app.get("/",(req,res) => {
-  res.send("hello mern");
-})
+//ミドルウェアの設定
+app.use("/app/users",userRoute);
+app.use("/app/auth",authRoute);
+app.use("/app/post",postRoute);
+
+// app.get("/",(req,res) => {
+//   res.send("hello mern");
+// });
 
 app.listen(PORT,() => {
   return console.log("サーバーが起動しました。")
-})
+});
